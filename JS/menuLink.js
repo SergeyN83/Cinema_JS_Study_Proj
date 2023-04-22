931e9394413344a9efc2ae6779ff37daeb094934
Sergei Novoselov <Sergey_N83@inbox.ru>
2023-04-22 19:38:45 +0300

    Add Trailers, Search, and Modal-youtube

--- JS/menuLink.js
+++ JS/menuLink.js
@@ -1,18 +1,13 @@
-import {
-    getPopular,
-    getTop,
-    getTrends
-} from './services.js';
+import { getPopular, getTop, getTrends } from './services.js';
 import renderCard from './renderCard.js';
 
-const title = document.querySelector('.other-films__title'),
-    filmWeek = document.querySelector('.film-week'),
-    getNav = document.querySelectorAll('.get-nav');
+const title = document.querySelector('.other-films__title');
+const filmWeek = document.querySelector('.film-week');
+const getNav = document.querySelectorAll('.get-nav');
 
 const menuLink = () => {
     getNav.forEach(nav => {
         nav.addEventListener('click', event => {
-
             const target = event.target.closest('.get-nav__link');
 
             if (target) {
@@ -20,30 +15,30 @@ const menuLink = () => {
 
                 filmWeek.style.display = 'none';
                 title.textContent = target.textContent;
-
+                //Тренды
                 if (target.classList.contains('get-nav__link_triends')) {
                     getTrends()
                         .then(data => renderCard(data.results));
                 }
-
+                //Популярные фильмы
                 if (target.classList.contains('get-nav__link_popular-movies')) {
                     getPopular('movie')
-                        .then(data => renderCard(data.results));
+                        .then(data => renderCard(data.results, 'movie'));
                 }
-
+                //Популярные сериалы
                 if (target.classList.contains('get-nav__link_popular-tv')) {
                     getPopular('tv')
-                        .then(data => renderCard(data.results));
+                        .then(data => renderCard(data.results, 'tv'));
                 }
-
+                //Топ фильмов
                 if (target.classList.contains('get-nav__link_top-movies')) {
                     getTop('movie')
-                        .then(data => renderCard(data.results));
+                        .then(data => renderCard(data.results, 'movie'));
                 }
-
+                //Топ сериалов
                 if (target.classList.contains('get-nav__link_top-tv')) {
                     getTop('tv')
-                        .then(data => renderCard(data.results));
+                        .then(data => renderCard(data.results, 'tv'));
                 }
             }
         });
--- JS/renderVideo.js
+++ JS/renderVideo.js
@@ -1,22 +1,31 @@
-import { getTrends } from "./services.js";
+import { getTrends, getVideo } from "./services.js";
 import renderCard from "./renderCard.js";
 
 const filmWeek = document.querySelector('.film-week');
 
-const firstRender = data => {
+const firstRender = (data, key ) => {
+
+    const {
+        vote_average: voteAverage,
+        backdrop_path: backdropPath,
+        name,
+        title,
+        original_name: originalName,
+        original_title: originalTitle
+    } = data;
 
     filmWeek.innerHTML = `
-        <div class="container film-week__container" data-rating="${data.vote_average}">
+        <div class="container film-week__container" data-rating="${voteAverage}">
             <div class="film-week__poster-wrapper">
                 <img class="film-week__poster" 
-                src="https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${data.backdrop_path}" 
-                alt="постер ${data.title || data.name}">
-                <p class="film-week__title_origin">${data.original_title || data.original_name}</p>
+                    src="https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${backdropPath}" alt="постер ${title || name}">
+                <p class="film-week__title_origin">${originalTitle || originalName}</p>
             </div>
-            <h2 class="film-week__title">${data.title || data.name}</h2>
-            <a class="film-week__watch-trailer tube" 
-                href="https://youtu.be/V0hagz_8L3M" aria-label="смотреть трейлер">
-            </a>
+            <h2 class="film-week__title">${title || name}</h2>
+            ${key ?
+                `<a class="film-week__watch-trailer tube" href="https://youtu.be/${key}" aria-label="смотреть трейлер"></a>` :
+                ''}
+
         </div>
     `;
 };
@@ -26,7 +35,10 @@ const renderVideo = async () => {
 
     const [ firstCard, ...otherCard ] = data.results;
     otherCard.length = 16;
-    firstRender(firstCard);
+
+    const video = await getVideo(firstCard.id, firstCard.media_type);
+
+    firstRender(firstCard, video.results[0]);
     renderCard(otherCard);
 };
 
